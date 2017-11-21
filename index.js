c0c03249483fa68e30499c75ddecf267eb7ba5f7
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-11-21 15:21:20 +0100

    basic react website setup

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var cli = require('cli');
-var options = cli.parse();
-console.log(JSON.stringify(cli));
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const port = process.env.PORT || 3000;
+const app = express();
+const router = express.Router();
+
+process.on('SIGINT', () => {
+	console.log(`About to exit`);
+	process.exit();
+});
+
+// serve static assets normally
+app.use(express.static(__dirname));
+
+app.get('*', function (request, response){
+	response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+app.listen(port)
+console.log("server started on port " + port);
---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const Button = require('../../stacked-example/lib/patterns/atoms/button/index.js').default;
+		return (
+			<div>
+				hello world
+				{Button({disabled: true})}
+			</div>
+		);
+	}
+}
+
+render(
+	<App />,
+	document.getElementById('app')
+);

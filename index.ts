b6ef3ca422c92cf754b9d4be2f1123d7fc2d737f
Thomas Jacob <thomas.jacob@sinnerschrader.com>
2017-11-21 13:41:39 +0100

    More d.ts tests

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import * as path from 'path';
 import * as optimist from 'optimist';
-import * as dtsParser from 'dts-parser';
+import * as readts from 'readts';
+import * as ts from 'typescript';
 
-let [styleGuidePath, projectName, pageName] = optimist.argv();
+let [npmCommand, styleGuidePath, projectName, pageName] = optimist.argv._;
 let pagePath = path.join(styleGuidePath, 'stacked', projectName, pageName + '.json');
 
-let declarationPath = path.join(styleGuidePath, 'pattern', 'atom', 'button');
+let declarationPath = path.join(styleGuidePath, 'lib', 'patterns', 'atoms', 'button', 'index.d.ts');
 
 console.log("Path: " + declarationPath);
+
 console.log("Pattern: ");
-console.log(dtsParser.parse(declarationPath));
+let tsConfig: ts.ParsedCommandLine = {options:{}, fileNames: [declarationPath], errors: []};
+console.log(new readts.Parser().parse(tsConfig));

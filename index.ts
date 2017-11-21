a4ad520f89354899cba9649ec7971bfcc2a5aa8e
Thomas Jacob <thomas.jacob@sinnerschrader.com>
2017-11-21 13:10:53 +0100

    Tried to read index.d.ts of example project using dts-parser

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-import * as cli from 'cli';
+import * as path from 'path';
+import * as optimist from 'optimist';
+import * as dtsParser from 'dts-parser';
 
-let options = cli.parse();
+let [styleGuidePath, projectName, pageName] = optimist.argv();
+let pagePath = path.join(styleGuidePath, 'stacked', projectName, pageName + '.json');
 
-console.log(JSON.stringify(cli));
+let declarationPath = path.join(styleGuidePath, 'pattern', 'atom', 'button');
 
+console.log("Path: " + declarationPath);
+console.log("Pattern: ");
+console.log(dtsParser.parse(declarationPath));

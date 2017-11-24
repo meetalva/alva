6538de67240e67fa95a7e3cbf8a63e376c63dfa8
Thomas Jacob <thomas.jacob@sinnerschrader.com>
2017-11-24 09:53:38 +0100

    Started layout (projects, patterns, preview, properties)

--- src/index.jsx
+++ src/index.jsx
@@ -1,81 +1,85 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import path from 'path';
-import fs from 'fs';
+import styled, { css } from 'styled-components';
+import Preview from './preview';
+
+
+const ColumnGroup = styled.div`
+	display: flex;
+	flex-direction: row;
+	height: 100%;
+`;
+
+const LeftColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex: 1 0 0px;
+	border: 1px solid #ccc;
+	box-shadow: 3px 0 10px 0 rgba(0,0,0,.25);
+`;
+
+const ProjectsPane = styled.div`
+	flex: 2 0 0px;
+	border-bottom: 1px solid #ccc;
+`;
+
+const PatternsPane = styled.div`
+	flex: 3 0 0px;
+`;
+
+const PreviewPane = styled.div`
+	flex: 2 0 0px;
+	padding: 10px;
+`;
+
+const PropertiesPane = styled.div`
+	flex: 1 0 0px;
+	border: 1px solid #ccc;
+	box-shadow: -3px 0 10px 0 rgba(0,0,0,.25);
+`;
+
+const List = styled.ul`
+`;
+
+const ListItem = styled.li`
+`;
+
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-
-		this.styleGuidePath = '../stacked-example';
-		this.projectName = 'my-project';
-		this.pageName = 'mypage';
-
-		this.pagePath = path.join(this.styleGuidePath, 'stacked', 'projects',
-			this.projectName, this.pageName + '.json');
-		this.patternFactories = {};
 	}
 
 	render() {
-		const pageModel = JSON.parse(fs.readFileSync(this.pagePath, 'utf8'));;
-		return this.createComponent(pageModel.root);
-	}
+		return (
+			<ColumnGroup>
+				<LeftColumn>
+					<ProjectsPane>
+						<List>
+							<ListItem active={true}>
+								My Project
+								<List>
+									<ListItem active={true}>
+									</ListItem>
+								</List>
+							</ListItem>
+						</List>
+					</ProjectsPane>
+					
+					<PatternsPane>
+						Hello world.
+					</PatternsPane>
+				</LeftColumn>
 
-	/**
-	 * Converts a JSON-serializable declaration of a pattern, primitive, or collection
-	 * into a React component (or primitive), deep-traversing through properties and children.
-	 * 
-	 * @param model The model, may be an object with 'patternSrc' property (a pattern declaration),
-	 * a primitive like a string, number, boolean, null/undefined, etc.,
-	 * or an array or object of such values.
-	 * @returns A React component in case of a pattern declaration, the primitive in case of a primitive,
-	 * or an array or object with values converted in the same manner, if an array resp. object is provided.
-	 */
-	createComponent(model, key) {
-		if (Array.isArray(model)) {
-			// Handle arrays by returning a new array with recursively processed elements.
-			return model.map((element, index) => {
-				return this.createComponent(element, index);
-			});
-		}
+				<PreviewPane>
+					<Preview />
+				</PreviewPane>
 
-		if (model === null || typeof model !== 'object') {
-			// Primitives stay primitives.
-			return model;
-		}
-
-		if (model['_type'] == 'pattern') {
-			// The model is a pattern declaration, create a React pattern component
-
-			// First, process the properties and children of the declaration recursively
-			const componentProps = this.createComponent(model.properties) || {};
-			componentProps.children = this.createComponent(model.children);
-			if (key != null) {
-				componentProps.key = key;
-			}
-
-			// Then, load the pattern factory
-			const patternFolder = path.join(this.styleGuidePath, 'lib', 'patterns', model.patternSrc);
-			const patternSrc = path.join(patternFolder, 'index.js');
-			let patternFactory = this.patternFactories[patternFolder];
-			if (patternFactory == null) {
-				console.log("Loading pattern '" + patternSrc + "'...");
-				patternFactory = require(patternSrc).default;
-				this.patternFactories[patternFolder] = patternFactory;
-			}
-
-			// Finally, build the component
-			return patternFactory(componentProps);
-		} else {
-			// The model is an object, but not a pattern declaration.
-			// Create a new object with recursively processed values.
-			var result = {};
-			Object.keys(model).forEach((key) => {
-				result[key] = this.createComponent(model[key]);
-			});
-			return result;
-		}
-
+				<PropertiesPane>
+					Hello world.
+				</PropertiesPane>
+			</ColumnGroup>
+		);
 	}
 }
 

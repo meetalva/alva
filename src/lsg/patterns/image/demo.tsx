251905f36a87ab214e3d6c6f5f810afc0a73283e
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-12-06 11:47:09 +0100

    styling(lsg): convert tabs to spaces

--- src/lsg/patterns/image/demo.tsx
+++ src/lsg/patterns/image/demo.tsx
@@ -2,17 +2,12 @@ import * as React from 'react';
 import Image from './index';
 
 const ImageDemo = () => {
-  // example with srcset and props in object
-  const image = {
-    alt: 'Gourgeously crafted alternate text',
-    src: 'https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif',
-  };
-  return (
-    <Image
-      alt={image.alt}
-      src={image.src}
-    />
-  );
+	// example with srcset and props in object
+	const image = {
+		alt: 'Gourgeously crafted alternate text',
+		src: 'https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif'
+	};
+	return <Image alt={image.alt} src={image.src} />;
 };
 
 export default ImageDemo;
--- src/lsg/patterns/image/index.tsx
+++ src/lsg/patterns/image/index.tsx
@@ -2,24 +2,17 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 export interface ImageProps {
-  alt: string;
-  className?: string;
-  src: string;
+	alt: string;
+	className?: string;
+	src: string;
 }
 
 const StyledImage = styled.img`
-  display: block;
+	display: block;
 `;
 
-const Image: React.StatelessComponent<ImageProps> = (props) => {
-
-  return (
-    <StyledImage
-      alt={props.alt}
-      className={props.className}
-      src={props.src}
-    />
-  );
+const Image: React.StatelessComponent<ImageProps> = props => {
+	return <StyledImage alt={props.alt} className={props.className} src={props.src} />;
 };
 
 export default Image;

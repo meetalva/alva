ac7dbd0db67a34415947943f48949b8dc796831e
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-12-06 17:21:36 +0100

    feat(lsg): new element pattern

---
+++ src/lsg/patterns/element/demo.tsx
@@ -0,0 +1,22 @@
+import { Element } from './index';
+import * as React from 'react';
+import styled from 'styled-components';
+
+const StyledTestDiv = styled.div`
+	flex-grow: 1;
+	max-width: 200px;
+	padding: 20px 10px;
+`;
+
+const ElementDemo: React.StatelessComponent<void> = (): JSX.Element => (
+	<div>
+		<StyledTestDiv>
+			<Element title="Element">Child</Element>
+		</StyledTestDiv>
+		<StyledTestDiv>
+			<Element active title="Active Element" />
+		</StyledTestDiv>
+	</div>
+);
+
+export default ElementDemo;

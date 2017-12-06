03e993ad9a37c1ccd5b30bfed6062ba722a3572c
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-12-06 11:40:25 +0100

    chore: move lsg in to src folder

--- patternplate.config.js
+++ patternplate.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   docs: ["docs/**/*.md"],
-  entry: ["lsg/lib/patterns/**/demo.js"],
+  entry: ["dist/lsg/patterns/**/demo.js"],
   render: "@patternplate/render-styled-components/render",
   mount: "@patternplate/render-styled-components/mount",
   ui: {

ce93c6ad76c50b5836e7ad5675f5274a81b83077
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-12-07 14:37:43 +0100

    feat(component): toggle for elements

---
+++ src/component/container/elementWrapper.tsx
@@ -0,0 +1,44 @@
+import Element from '../../lsg/patterns/element';
+import * as React from 'react';
+
+export interface ElementWrapperState {
+	open?: boolean;
+}
+
+export interface ElementWrapperProps {
+	active?: boolean;
+	open?: boolean;
+	title: string;
+}
+
+export class ElementWrapper extends React.Component<ElementWrapperProps, ElementWrapperState> {
+	public constructor(props: ElementWrapperProps) {
+		super(props);
+
+		this.state = {
+			open: this.props.open
+		};
+
+		this.handleIconClick = this.handleIconClick.bind(this);
+	}
+
+	private handleIconClick(): void {
+		this.setState({
+			open: !this.state.open
+		});
+	}
+
+	public render(): JSX.Element {
+		const { active, children, title } = this.props;
+		return (
+			<Element
+				title={title}
+				open={!this.state.open}
+				active={active}
+				handleIconClick={this.handleIconClick}
+			>
+				{children}
+			</Element>
+		);
+	}
+}

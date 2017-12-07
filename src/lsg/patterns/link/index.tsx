6c31230b54692d6fd17e307be35841c4931fbdc7
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2017-12-07 16:58:53 +0100

    feat(lsg): add props and create demo content for link component

--- src/lsg/patterns/link/index.tsx
+++ src/lsg/patterns/link/index.tsx
@@ -1,4 +1,5 @@
 import { Color } from '../colors';
+import { fonts } from '../fonts';
 import * as React from 'react';
 import styled, { StyledComponentClass } from 'styled-components';
 
@@ -7,17 +8,33 @@ export interface LinkProps {
 	color?: Color;
 	disabled?: boolean;
 	onClick?: React.MouseEventHandler<HTMLElement>;
+	uppercase?: boolean;
 }
 
 const StyledLink: StyledComponentClass<LinkProps, {}> = styled.a`
-	color: ${(props: LinkProps) => props.color
-		? props.color.toString()
-		: 'inherit'
+	font-family: ${fonts().NORMAL_FONT};
+	${(props: LinkProps) => props.color
+		? `color: ${props.color.toString()}`
+		: 'color: inherit'
 	};
+	${(props: LinkProps) => props.onClick
+		? 'cursor: pointer;'
+		: 'cursor: auto;'
+	}
+	${(props: LinkProps) => props.uppercase
+		? 'text-transform: uppercase;'
+		: ''
+	}
 `;
 
 const Link: React.StatelessComponent<LinkProps> = props => (
-	<StyledLink>
+	<StyledLink
+		className={props.className}
+		color={props.color}
+		disabled={props.disabled}
+		onClick={props.onClick}
+		uppercase={props.uppercase}
+	>
 		{props.children}
 	</StyledLink>
 );

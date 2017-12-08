82c3701ce68fa18a07eb01116c27151db1fcb04f
dangoo <daniel.gooss@sinnerschrader.com>
2017-12-08 17:09:56 +0100

    components: Included new chrome

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -9,7 +9,12 @@ let win: BrowserWindow | undefined;
 
 function createWindow(): void {
 	// Create the browser window.
-	win = new BrowserWindow({ width: 1800, height: 800 });
+	win = new BrowserWindow({
+		width: 1800,
+		height: 800,
+		titleBarStyle: 'hiddenInset',
+		title: 'Stacked'
+	});
 
 	// and load the index.html of the app.
 	win.loadURL(

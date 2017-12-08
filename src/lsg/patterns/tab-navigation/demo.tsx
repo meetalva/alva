9fd4013cbe9da9a3d7f736fbc9af52cbc9f98752
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2017-12-08 12:31:37 +0100

    feat(lsg): introduce tab-navigation component

---
+++ src/lsg/patterns/tab-navigation/demo.tsx
@@ -0,0 +1,17 @@
+import TabNavigation, {TabNavigationItem} from './index';
+import * as React from 'react';
+
+const TabNavigationDemo = () => (
+	<TabNavigation>
+		<TabNavigationItem
+			active={true}
+			tabText="Patterns"
+		/>
+		<TabNavigationItem
+			active={false}
+			tabText="Properties"
+		/>
+	</TabNavigation>
+);
+
+export default TabNavigationDemo;

45029ac493a5632aea1c4399546213412659cdc0
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-12-11 14:49:17 +0100

    feat(lsg): margins for layout pattern

--- src/lsg/patterns/layout/index.tsx
+++ src/lsg/patterns/layout/index.tsx
@@ -1,22 +1,26 @@
+import { getSpace, Size } from '../space/index';
 import * as React from 'react';
 import styled from 'styled-components';
 
 export interface LayoutProps {
 	className?: string;
 	directionVertical?: boolean;
+	hasMargins?: boolean;
 }
 
 const StyledLayout = styled.div`
 	display: flex;
 	justify-content: space-between;
-	${(props: LayoutProps) => props.directionVertical
-		? 'flex-direction: column;'
-		: ''
-	}
+	${(props: LayoutProps) => (props.directionVertical ? 'flex-direction: column;' : '')};
+	${(props: LayoutProps) => (props.hasMargins ? `margin: 0 ${getSpace(Size.L)}px` : '')};
 `;
 
 const Layout: React.StatelessComponent<LayoutProps> = props => (
-	<StyledLayout className={props.className} directionVertical={props.directionVertical}>
+	<StyledLayout
+		className={props.className}
+		directionVertical={props.directionVertical}
+		hasMargins={props.hasMargins}
+	>
 		{props.children}
 	</StyledLayout>
 );

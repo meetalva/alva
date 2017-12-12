9ebda51143a72fd7303e8cca006ffceb3a131ec4
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2017-12-12 17:31:44 +0100

    feat: move global styles to lsg

---
+++ src/lsg/patterns/global-styles/demo.tsx
@@ -0,0 +1,13 @@
+import globalStyles from './index';
+import * as React from 'react';
+
+const DemoGlobal: React.StatelessComponent<void> = (): JSX.Element => {
+	globalStyles();
+	return (
+		<section>
+		Global Styles
+		</section>
+	);
+};
+
+export default DemoGlobal;

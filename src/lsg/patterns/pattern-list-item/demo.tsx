3eca263580e4d3607bd117cdcf97bb4fcf479f0a
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2017-12-12 19:08:38 +0100

    feat(lsg): introduce demo container component

--- src/lsg/patterns/pattern-list-item/demo.tsx
+++ src/lsg/patterns/pattern-list-item/demo.tsx
@@ -1,15 +1,16 @@
+import DemoContainer from '../demo-container';
 import { IconName, IconRegistry } from '../icons';
 import PatternListItem from './index';
 import * as React from 'react';
 
 const PatternListItemDemo: React.StatelessComponent<void> = (): JSX.Element => (
-	<div>
+	<DemoContainer title="Pattern List Item">
 		<PatternListItem>
 			Copy
 		</PatternListItem>
 
 		<IconRegistry names={IconName}/>
-	</div>
+	</DemoContainer>
 );
 
 export default PatternListItemDemo;

2bad46f625a2c173840dbd3cae43d138a36f5932
Tilman Frick <tilman.frick@sinnerschrader.com>
2017-12-15 16:42:38 +0100

    fix(lsg): broken intendantion

--- src/lsg/patterns/property-items/enum-item/index.tsx
+++ src/lsg/patterns/property-items/enum-item/index.tsx
@@ -19,23 +19,23 @@ const StyledEnumItem = styled.div`
 `;
     
 const StyledSelect = styled.select`
-    appearance: none;
-    border: none;
-    border-radius: 0;
-    font-size: 1em;
-    width: 100%;
-    font-family: ${fonts().NORMAL_FONT};
+	appearance: none;
+	border: none;
+	border-radius: 0;
+	font-size: 1em;
+	width: 100%;
+	font-family: ${fonts().NORMAL_FONT};
 	color: ${colors.grey36.toString()};
-    font-size: 15px;
-    border-bottom: 1px solid ${colors.grey70.toString()};
-    padding-bottom: ${getSpace(Size.M)/2}px;
-    margin-bottom: ${getSpace(Size.XXL)}px;
-    
-    &:focus {
-        outline: none;
-        border-color: ${colors.blue.toString()};
-        color: ${colors.black.toString()};
-    }
+	font-size: 15px;
+	border-bottom: 1px solid ${colors.grey70.toString()};
+	padding-bottom: ${getSpace(Size.M)/2}px;
+	margin-bottom: ${getSpace(Size.XXL)}px;
+	
+	&:focus {
+		outline: none;
+		border-color: ${colors.blue.toString()};
+		color: ${colors.black.toString()};
+	}
 `;
 
 const StyledLabel = styled.span`

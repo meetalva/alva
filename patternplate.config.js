199881a8429d6b5de4e9a4222849defc26af1fd8
Thomas Jacob <thomas.jacob@sinnerschrader.com>
2017-12-15 11:33:54 +0100

    chore(build): switched roles of build and dist directories, removed non-standalone scripts

--- patternplate.config.js
+++ patternplate.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	docs: ['docs/**/*.md'],
-	entry: ['dist/lsg/patterns/**/demo.js'],
+	entry: ['build/lsg/patterns/**/demo.js'],
 	render: '@patternplate/render-styled-components/render',
 	mount: '@patternplate/render-styled-components/mount',
 	ui: {

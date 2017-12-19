389c098fa2b22d76e990c1e1f50e515d59001b79
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-12-19 15:45:13 +0100

    feat(menu): add functionality to also create a folder while selecting a space for the creation of th

--- src/electron/menu.ts
+++ src/electron/menu.ts
@@ -21,7 +21,7 @@ export function createMenu(store: Store): void {
 
 						const designkitPath = PathUtils.join(appPath, 'build', 'designkit');
 						console.log(`Design kit path is: ${designkitPath}`);
-						dialog.showOpenDialog({ properties: ['openDirectory'] }, filePaths => {
+						dialog.showOpenDialog({ properties: ['openDirectory', 'createDirectory'] }, filePaths => {
 							if (filePaths.length <= 0) {
 								return;
 							}

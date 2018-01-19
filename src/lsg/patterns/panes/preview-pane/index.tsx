7b3bfae23cf5ad49239edc0b58241939fde9c41f
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2018-01-19 19:37:30 +0100

    feat: add ability to define a custom preview frame html wrapper

--- src/lsg/patterns/panes/preview-pane/index.tsx
+++ src/lsg/patterns/panes/preview-pane/index.tsx
@@ -2,6 +2,10 @@ import { colors } from '../../colors';
 import * as React from 'react';
 import styled from 'styled-components';
 
+export interface PreviewPaneProps {
+	previewFrame?: string;
+}
+
 const StyledPreviewPane = styled.div`
 	flex-grow: 1;
 	flex-shrink: 0;
@@ -11,13 +15,21 @@ const StyledPreviewPane = styled.div`
 	overflow: hidden;
 `;
 
-const PreviewPane: React.StatelessComponent = props => (
-	<StyledPreviewPane
-		dangerouslySetInnerHTML={{
-			__html:
-				'<webview id="preview" style="height: 100%; border-radius: 6px 6px 0 0; overflow: hidden;" src="./preview.html" partition="electron" nodeintegration />'
-		}}
-	/>
-);
+export default class PreviewPane extends React.Component<PreviewPaneProps> {
+	public constructor(props: PreviewPaneProps) {
+		super(props);
+	}
 
-export default PreviewPane;
+	public render(): JSX.Element {
+		const { previewFrame } = this.props;
+
+		return (
+			<StyledPreviewPane
+				dangerouslySetInnerHTML={{
+					__html: `<webview id="preview" style="height: 100%; border-radius: 6px 6px 0 0; overflow: hidden;" src="${previewFrame ||
+						'./preview.html'}" preload="../../build/component/preview.js" partition="electron" nodeintegration />`
+				}}
+			/>
+		);
+	}
+}

71544cc03dd16b66ba7b2aa8596101bf713810ca
Lasse Kuechler <lasse.kuechler@sinnerschrader.com>
2018-02-17 15:01:01 +0100

    chore(release): add pre and post processing for distribution command

---
+++ scripts/dist-preprocess.js
@@ -0,0 +1,26 @@
+const { execSync } = require('child_process');
+
+shelljs.execSync('standard-version --no-verify');
+console.info('Clean build enviroment.');
+execSync('npm run clean-build');
+
+console.info('Starting to copy designkit to build folder.');
+execSync(`
+	mkdir build/designkit &&
+	cp -r ../designkit/patterns ../designkit/alva ../designkit/*.* build/designkit/
+`);
+
+console.info('Install node packages for designkit.');
+execSync(`
+	npm i &&
+	npm run build &&
+	rm -rf node_modules &&
+	npm i --production
+`, {
+		cwd: 'build/designkit'
+	}
+);
+
+console.info('Distrubution preperation finished.');
+console.info('Starting Alva application build.');
+execSync('npm run electron-build');

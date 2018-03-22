9fdad0b8a70afa39b20ae3bf811a86abb2b82c30
Thomas Jacob <thomas.jacob@sinnerschrader.com>
2018-03-22 11:01:11 +0100

    feat(preview): pattern error handling (fixes #271)

--- src/component/presentation/react/render.tsx
+++ src/component/presentation/react/render.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-
 import { PreviewApp } from './preview';
 import { HighlightElementFunction } from '../../preview';
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
 import { Store } from '../../../store/store';
 
 export const renderReact = (store: Store, highlightElement: HighlightElementFunction) => {

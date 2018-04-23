f0ae1d521b64ef59521fcb40a93dc01943be7895
Julius Walther <julius.walther@sinnerschrader.com>
2018-04-23 09:56:33 +0200

    fix(object-property): infinite loop on type recursion

--- src/store/styleguide/property/object-property.ts
+++ src/store/styleguide/property/object-property.ts
@@ -1,5 +1,7 @@
 import { Property } from './property';
 
+export type PropertyResolver = () => Property[];
+
 /**
  * An object property is a property that supports objects with nested property values.
  * The properties field declares the types of each object element.
@@ -11,7 +13,9 @@ export class ObjectProperty extends Property {
 	/**
 	 * The nested properties this property supports in its object values.
 	 */
-	private properties: Map<string, Property> = new Map();
+	private properties?: Map<string, Property>;
+
+	private propertyResolver?: PropertyResolver;
 
 	/**
 	 * Creates a new object property.
@@ -39,7 +43,7 @@ export class ObjectProperty extends Property {
 	 * @return The nested properties this property supports.
 	 */
 	public getProperties(): Property[] {
-		return Array.from(this.properties.values());
+		return Array.from(this.resolveProperties().values());
 	}
 
 	/**
@@ -48,7 +52,7 @@ export class ObjectProperty extends Property {
 	 * @return The nested property if the ID was found.
 	 */
 	public getProperty(id: string): Property | undefined {
-		return this.properties.get(id);
+		return this.resolveProperties().get(id);
 	}
 
 	/**
@@ -58,16 +62,30 @@ export class ObjectProperty extends Property {
 		return 'object';
 	}
 
+	private resolveProperties(): Map<string, Property> {
+		if (!this.properties) {
+			if (!this.propertyResolver) {
+				throw new Error('property resolver is not set');
+			}
+
+			const resolvedProperties = this.propertyResolver();
+			const properties = new Map();
+
+			resolvedProperties.forEach(property => properties.set(property.getId(), property));
+
+			this.properties = properties;
+		}
+
+		return this.properties;
+	}
+
 	/**
 	 * Sets The nested properties this property supports in its object values.<br>
 	 * <b>Note:</b> This method should only be called from the pattern parsers.
 	 * @param properties The nested properties this property supports.
 	 */
-	public setProperties(properties: Property[]): void {
-		this.properties = new Map();
-		for (const property of properties) {
-			this.properties.set(property.getId(), property);
-		}
+	public setPropertyResolver(propertyResolver: PropertyResolver): void {
+		this.propertyResolver = propertyResolver;
 	}
 
 	/**

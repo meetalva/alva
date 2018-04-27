adbe1f69960a1567435b64b40d54df139ded8cf3
Tilman Frick <tilman.frick@sinnerschrader.com>
2018-04-27 23:04:31 +0200

    feat: add icons to boolean toggle

--- src/lsg/patterns/property-items/boolean-item/index.tsx
+++ src/lsg/patterns/property-items/boolean-item/index.tsx
@@ -22,9 +22,8 @@ const StyledBooleanItem = styled.label`
 	margin-bottom: ${getSpace(Size.M)}px;
 `;
 
-const indicatorWidth = 42;
-const indicatorHeight = 24;
-const indicatorBorderWidth = 1;
+const indicatorWidth = 48;
+const indicatorHeight = 27;
 
 const StyledIndicator = styled.span`
 	position: relative;
@@ -32,31 +31,36 @@ const StyledIndicator = styled.span`
 	width: ${indicatorWidth}px;
 	height: ${indicatorHeight}px;
 	border-radius: ${indicatorHeight / 2}px;
-	background: ${colors.grey90.toString()};
 	box-sizing: border-box;
+	box-shadow: inset 0 0 0 1px ${colors.grey60.toString()};
+	transition: all ease-in-out 0.15s;
+	user-select: none;
 
 	&:after {
-		content: '';
+		position: absolute;
+		content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><path d="M6,6L4.5,7.5L6,6L4.5,4.5L6,6z M6,6l1.5-1.5L6,6l1.5,1.5L6,6z" fill="none" stroke="${colors.grey60.toString()}" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" /></svg>');
 		display: block;
 		width: ${indicatorHeight}px;
 		height: ${indicatorHeight}px;
-		box-shadow: 0 0 1.5px ${indicatorBorderWidth}px ${colors.grey80.toString()};
-		transform: translateX(0px);
+		transform: translateX(${indicatorWidth - indicatorHeight}px);
 		border-radius: 100%;
 		background: ${colors.white.toString()};
-		transition: all ease-in-out 0.25s;
+		transition: all ease-in-out 0.15s;
 		box-sizing: border-box;
+		box-shadow: inset 0 0 0 1px ${colors.grey60.toString()};
 	}
 	${(props: IndicatorProps) =>
 		props.checked
 			? `
-			background: ${colors.blue40.toString()};
-			box-shadow: inset 0 0 0 ${indicatorBorderWidth}px ${colors.blue40.toString()};
+			background: ${colors.blue80.toString()};
+			box-shadow: inset 0 0 0 0.5px ${colors.blue40.toString()};
 
 			&:after {
-				transform: translateX(${indicatorWidth - indicatorHeight}px);
+				content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><polyline points="3.8,6 5.2,7.5 8.2,4.5 " fill="none" stroke="${colors.blue.toString()}" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></polyline></svg>');
+				transform: translateX(0px);
 				background: ${colors.white.toString()};
 				border-color: ${colors.blue40.toString()};
+				box-shadow: inset 0 0 0 1px ${colors.blue40.toString()};
 			}
 		`
 			: ''};
@@ -66,9 +70,14 @@ const StyledLabel = styled.span`
 	display: inline-block;
 	font-size: 12px;
 	font-family: ${fonts().NORMAL_FONT};
-	color: ${colors.grey36.toString()};
+	color: ${colors.grey60.toString()};
 	padding: ${getSpace(Size.XS)}px 0;
 	width: 30%;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis
+	user-select: none;
+	cursor: default;
 `;
 
 const StyledInput = styled.input`

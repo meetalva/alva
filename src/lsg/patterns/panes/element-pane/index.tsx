344f2cb95d66933e87fd0b3eda2372f8c19637d7
Tilman Frick <tilman.frick@sinnerschrader.com>
2018-04-27 12:40:09 +0200

    fix: add element overflow gradient

--- src/lsg/patterns/panes/element-pane/index.tsx
+++ src/lsg/patterns/panes/element-pane/index.tsx
@@ -1,3 +1,4 @@
+import { colors } from '../../colors';
 import * as React from 'react';
 import { getSpace, Size } from '../../space';
 import styled from 'styled-components';
@@ -8,6 +9,21 @@ const StyledElementPane = styled.div`
 	overflow: scroll;
 	padding-top: ${getSpace(Size.M)}px;
 	padding-bottom: ${getSpace(Size.XL)}px;
+
+	&::after {
+		content: '';
+		position: sticky;
+		bottom: 0;
+		display: block;
+		width: 100%;
+		height: ${getSpace(Size.XXXL)}px;
+		background: linear-gradient(
+			to bottom,
+			${colors.grey97.toString('rgb', { alpha: 0 })},
+			${colors.grey97.toString('rgb', { alpha: 1 })}
+		);
+		z-index: 15;
+	}
 `;
 
 const ElementPane: React.StatelessComponent = props => (

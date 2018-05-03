c808cb84621ef4e4f2924aaa604582e2f28a7583
Mario Nebl <hello@mario-nebl.de>
2018-05-04 00:00:53 +0200

    fix: handle empty element name commands

--- src/store/command/element-name-command.ts
+++ src/store/command/element-name-command.ts
@@ -25,7 +25,7 @@ export class ElementNameCommand extends ElementCommand {
 	public constructor(element: PageElement, name: string) {
 		super(element);
 
-		this.name = name;
+		this.name = name.length === 0 ? element.getName({ unedited: true }) : name;
 		this.previousName = element.getName({ unedited: true });
 
 		if (!this.pageId) {

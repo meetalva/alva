d7aec31293ddbca7164aff375667ac9639f0741d
Mario Nebl <hello@mario-nebl.de>
2018-05-10 12:45:28 +0200

    feat: implement basic edit history for pages

---
+++ src/store/command/page-add-command.ts
@@ -0,0 +1,46 @@
+import { Command } from './command';
+import { Page } from '../page';
+import { Project } from '../project';
+import * as Types from '../types';
+import { ViewStore } from '../view-store';
+
+export class PageAddCommand extends Command {
+	private page: Page;
+	private project: Project;
+
+	private constructor(init: { page: Page; project: Project }) {
+		super();
+		this.page = init.page;
+		this.project = init.project;
+	}
+
+	public static create(init: { page: Page; project: Project }): PageAddCommand {
+		return new PageAddCommand(init);
+	}
+
+	public execute(): boolean {
+		const store = ViewStore.getInstance();
+
+		this.project.addPage(this.page);
+		store.setActivePage(this.page);
+		return true;
+	}
+
+	public getType(): string {
+		return 'page-create';
+	}
+
+	public undo(): boolean {
+		const store = ViewStore.getInstance();
+		const index = this.project.getPageIndex(this.page);
+
+		if (index === 0) {
+			store.unsetActivePage();
+			store.setActiveView(Types.AlvaView.Pages);
+		} else {
+			store.setActivePageByIndex(index - 1);
+		}
+
+		return this.project.removePage(this.page);
+	}
+}

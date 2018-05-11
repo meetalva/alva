a37896f19f5fbd7be22df5b48cbbbccccedd0066
Tilman Frick <git@tilman.io>
2018-05-11 14:53:14 +0200

    feat: update styling of property pane (#436)
    * feat(component): update styling of string property
    * feat(component): update styling of boolean property
    * feat(component): update styling of enum property

--- src/components/property-items/string-item/index.tsx
+++ src/components/property-items/string-item/index.tsx
@@ -1,5 +1,6 @@
 import { colors } from '../../colors';
 import { fonts } from '../../fonts';
+import { PropertyLabel } from '../property-label';
 import * as React from 'react';
 import { getSpace, SpaceSize } from '../../space';
 import styled from 'styled-components';
@@ -12,46 +13,42 @@ export interface StringItemProps {
 	value?: string;
 }
 
-const StyledStringItem = styled.div`
+const StyledStringItem = styled.label`
+	display: flex;
+	align-items: center;
 	width: 100%;
-`;
-
-const StyledLabel = styled.span`
-	display: block;
-	margin-bottom: ${getSpace(SpaceSize.XS)}px;
-	font-size: 12px;
-	font-family: ${fonts().NORMAL_FONT};
-	color: ${colors.grey36.toString()};
+	margin-bottom: ${getSpace(SpaceSize.S)}px;
 `;
 
 const StyledInput = styled.input`
-	display: block;
+	display: inline-block;
 	box-sizing: border-box;
-	width: 100%;
-	text-overflow: ellipsis;
-	border: none;
-	border-bottom: 1px solid transparent;
-	background: transparent;
+	width: 70%;
+	height: 30px;
+	padding: ${getSpace(SpaceSize.XS)}px ${getSpace(SpaceSize.S)}px;
+	border: 1px solid ${colors.grey90.toString()};
+	@media screen and (-webkit-min-device-pixel-ratio: 2) {
+		border-width: 0.5px;
+	}
+	border-radius: 3px;
+	background: ${colors.white.toString()};
+	color: ${colors.grey20.toString()};
 	font-family: ${fonts().NORMAL_FONT};
 	font-size: 15px;
-	padding-bottom: ${getSpace(SpaceSize.M) / 2}px;
-	color: ${colors.grey36.toString()};
-	margin-bottom: ${getSpace(SpaceSize.L)}px;
-	transition: all 0.2s;
-
+	text-overflow: ellipsis;
+	transition: border-color 0.1s, box-shadow 0.1s, color 0.1s;
 	::-webkit-input-placeholder {
 		color: ${colors.grey60.toString()};
 	}
-
 	&:hover {
 		color: ${colors.black.toString()};
 		border-color: ${colors.grey60.toString()};
 	}
-
 	&:focus {
 		outline: none;
 		border-color: ${colors.blue40.toString()};
 		color: ${colors.black.toString()};
+		box-shadow: 0 0 3px ${colors.blue.toString('rgb', { alpha: 0.4 })};
 	}
 `;
 

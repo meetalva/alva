7baa3757b0941542e5a46c7141cc9273810f1483
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-05-15 17:31:36 +0200

    refactor(highlight): implement tests for the hightlight component

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
 	transform: {
 		"^.+\\.tsx?$": "ts-jest"
 	},
-	testRegex: "\/test\/[^\.]*\.tsx?$",
+	testRegex: "(\\.|/)(test|spec)\\.(jsx?|tsx?)$",
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
 	roots: ["src"],
 	testEnvironment: "node"
 };
-

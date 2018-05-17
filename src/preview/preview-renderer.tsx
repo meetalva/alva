f37121803ef240301bfc3a3b12ced049196dfb8e
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-05-17 12:43:09 +0200

    refactor(resize): remove resize factory

--- src/preview/preview-renderer.tsx
+++ src/preview/preview-renderer.tsx
@@ -6,7 +6,6 @@ import * as MobXReact from 'mobx-react';
 import { PreviewStore } from './preview';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { ResizeFactory } from './resize';
 
 // TODO: Produces a deprecation warning, find a way
 // to dedupe MobX when upgrading to 4.x
@@ -80,7 +79,6 @@ class PreviewApplication extends React.Component {
 
 		if (props.store.elementId) {
 			props.highlight.show();
-			console.log('@@@@@@it goes inhere too', props.store.elementId);
 		} else {
 			props.highlight.hide();
 		}
@@ -129,25 +127,38 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
 @MobXReact.inject('getComponent', 'store', 'highlight')
 @MobXReact.observer
 class PreviewComponent extends React.Component<PreviewComponentProps> {
-	public componentDidMount(): void {
-		const props = this.props as InjectedPreviewComponentProps;
-		const resize = ResizeFactory();
-		const setSize = () => {
-			if (props.uuid === props.store.elementId) {
-				const node = ReactDom.findDOMNode(this);
-				props.highlight.setSize(node as Element);
-			}
-		};
-
-		resize.register(setSize);
+	public constructor(props: InjectedPreviewComponentProps) {
+		super(props);
+		this.handleResize = this.handleResize.bind(this);
 	}
+
+	public componentDidMount(): void {
+		window.addEventListener('resize', this.handleResize);
+	}
+
 	public componentDidUpdate(): void {
 		const props = this.props as InjectedPreviewComponentProps;
-
 		if (props.uuid === props.store.elementId) {
 			const node = ReactDom.findDOMNode(this);
 			if (node) {
 				props.highlight.setSize(node as Element);
+			}
+		}
+	}
+
+	public componentWillUnmount(): void {
+		window.removeEventListener('resize', this.handleResize);
+	}
+
+	private handleResize(): void {
+		const props = this.props as InjectedPreviewComponentProps;
+		if (props.uuid !== props.store.elementId) {
+			return;
+		} else {
+			const node = ReactDom.findDOMNode(this);
+			if (node) {
+				props.highlight.setSize(node as Element);
+				window.requestAnimationFrame(this.handleResize);
 			}
 		}
 	}

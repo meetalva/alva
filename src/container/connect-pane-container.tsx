149da8a8bedaca65e5cfb887299854fa9b178125
Mario Nebl <mario.nebl@sinnerschrader.com>
2018-05-20 22:24:26 +0200

    style: use consistent quotes

--- src/container/connect-pane-container.tsx
+++ src/container/connect-pane-container.tsx
@@ -8,12 +8,13 @@ export interface ConnectPaneProps {
 
 export const ConnectPaneContainer: React.SFC<ConnectPaneProps> = props => (
 	<ConnectLibrary
-		headline={'Connect to a Library'}
-		description={
-			'To build prototypes with already existing components, connect to your React component	library first.'
-		}
-		primaryButton={'Connect'}
-		secondaryButton={'Download Alva Example'}
+		headline="Connect to a Library"
+		description={`
+			To build prototypes with already existing components,
+			connect to your React component	library first.
+		`}
+		primaryButton="Connect"
+		secondaryButton="Download Alva Example"
 		onPrimaryButtonClick={props.onPrimaryButtonClick}
 		onSecondaryButtonClick={props.onSecondaryButtonClick}
 	/>

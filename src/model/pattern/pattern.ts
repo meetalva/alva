c1f1e5a91c7338fec1f3b0023a1cd8fff3e3d04e
Mario Nebl <mario.nebl@sinnerschrader.com>
2018-05-20 21:56:24 +0200

    refactor: introduce context id to identify patterns across versions

--- src/model/pattern/pattern.ts
+++ src/model/pattern/pattern.ts
@@ -6,9 +6,6 @@ import { PatternSlot } from './pattern-slot';
 import * as Types from '../types';
 import * as uuid from 'uuid';
 
-// tslint:disable-next-line:no-duplicate-imports
-// import { PatternPropertyType } from '../types';
-
 export type PatternType = SyntheticPatternType | ConcretePatternType;
 
 export enum SyntheticPatternType {
@@ -26,7 +23,6 @@ export interface PatternInit {
 	exportName?: string;
 	id?: string;
 	name: string;
-	path: string;
 	propertyIds?: string[];
 	slots?: PatternSlot[];
 	type: PatternType;
@@ -36,74 +32,30 @@ export interface PatternContext {
 	patternLibrary: PatternLibrary;
 }
 
-/**
- * A pattern represents a reusable, styled component (e.g. a React component) of the styleguide.
- * Patterns are parsed from the lib folder of the styleguide, supporting a set of properties
- * of various types, like strings, numbers, page elements, etc.
- * The designer then creates page elements within a page using these patterns as a basis.
- * Patterns may be structured into folders (like 'atoms', 'modules', etc.).
- * Depending on the styleguide analyzer used, various styleguide formats are supported,
- * e.g. Patternplate.
- */
 export class Pattern {
-	/**
-	 * The name of the export in the JavaScript implementation of the pattern.
-	 * For default export, the value is 'default'.
-	 */
+
+	private contextId: string;
+
 	private exportName: string;
 
-	/**
-	 * The folder containing the pattern.
-	 */
 	private folder: PatternFolder;
 
-	/**
-	 * The ID of the pattern. How this is generated is completely up to the styleguide analyzer
-	 * that creates the pattern (and does not necessarily represent the file path).
-	 */
 	private id: string;
 
-	/**
-	 * The human-readable name of the pattern.
-	 * In the frontend, to be displayed instead of the ID.
-	 */
 	private name: string;
-
-	/**
-	 * The absolute path to the JavaScript implementation of the pattern,
-	 * used to preview page elements.
-	 */
-	private path: string;
 
 	private patternLibrary: PatternLibrary;
 
-	/**
-	 * The properties this pattern supports.
-	 */
 	private propertyIds: string[];
 
-	/**
-	 * The slots this pattern supports
-	 */
 	private slots: PatternSlot[];
 
 	private type: PatternType;
 
-	/**
-	 * Creates a new pattern.
-	 * @param id The ID of the pattern. How this is generated is completely up to the styleguide analyzer
-	 * that creates the pattern (and does not necessarily represent the file path).
-	 * @param name The human-readable name of the pattern.
-	 * @param implementationPath The absolute path to the JavaScript implementation of the pattern,
-	 * used to preview page elements.
-	 * @param exportName The name of the export in the JavaScript implementation of the pattern.
-	 * For default export, the value is 'default'.
-	 */
 	public constructor(init: PatternInit, context: PatternContext) {
 		this.exportName = init.exportName || 'default';
 		this.id = init.id || uuid.v4();
 		this.name = AlvaUtil.guessName(init.name);
-		this.path = init.path || '';
 		this.patternLibrary = context.patternLibrary;
 		this.propertyIds = init.propertyIds || [];
 		this.type = init.type;
@@ -123,7 +75,6 @@ export class Pattern {
 				exportName: serialized.exportName,
 				id: serialized.id,
 				name: serialized.name,
-				path: serialized.path,
 				propertyIds: serialized.propertyIds,
 				slots: serialized.slots.map(slot => PatternSlot.from(slot)),
 				type: stringToType(serialized.type)
@@ -132,19 +83,10 @@ export class Pattern {
 		);
 	}
 
-	/**
-	 * Adds a slot to this pattern. This method is called by the analyzer only.
-	 * @param name The slot to add.
-	 */
 	public addSlot(slot: PatternSlot): void {
 		this.slots.push(slot);
 	}
 
-	/**
-	 * Returns the name of the export in the JavaScript implementation of the pattern.
-	 * For default export, the value is 'default'.
-	 * @return The name of the export.
-	 */
 	public getExportName(): string {
 		return this.exportName;
 	}
@@ -153,43 +95,20 @@ export class Pattern {
 		return this.folder;
 	}
 
-	/**
-	 * Returns the ID of the pattern. How this is generated is completely up to the
-	 * styleguide analyzer that creates the pattern (and does not necessarily represent
-	 * the file path).
-	 * @return The ID of the pattern.
-	 */
 	public getId(): string {
 		return this.id;
 	}
 
-	/**
-	 * Returns the human-readable name of the pattern.
-	 * In the frontend, to be displayed instead of the ID.
-	 * @return The human-readable name of the pattern.
-	 */
 	public getName(): string {
 		return this.name;
 	}
 
-	public getPath(): string {
-		return this.path;
-	}
-
-	/**
-	 * Returns the properties this pattern supports.
-	 * @return The properties this pattern supports.
-	 */
 	public getProperties(): PatternProperty.AnyPatternProperty[] {
 		return this.propertyIds
 			.map(propertyId => this.patternLibrary.getPatternPropertyById(propertyId))
 			.filter((p): p is PatternProperty.AnyPatternProperty => typeof p !== 'undefined');
 	}
 
-	/**
-	 * Returns the slots this pattern supports.
-	 * @return The slots this pattern supports.
-	 */
 	public getSlots(): PatternSlot[] {
 		return this.slots;
 	}
@@ -200,10 +119,10 @@ export class Pattern {
 
 	public toJSON(): Types.SerializedPattern {
 		return {
+			contextId: this.contextId,
 			exportName: this.exportName,
 			id: this.id,
 			name: this.name,
-			path: this.path,
 			propertyIds: this.propertyIds,
 			slots: this.slots.map(slot => slot.toJSON()),
 			type: this.type
@@ -227,28 +146,3 @@ const stringToType = (input: string): PatternType => {
 			throw new Error(`Unknown pattern type ${input}`);
 	}
 };
-
-// const P = PatternProperty;
-
-/* const unserializeProperty = (
-	serialized: Types.SerializedPatternProperty
-): PatternProperty.PatternProperty => {
-	switch (serialized.type) {
-		case PatternPropertyType.Asset:
-			return P.PatternAssetProperty.from(serialized);
-		case PatternPropertyType.Boolean:
-			return P.PatternBooleanProperty.from(serialized);
-		case PatternPropertyType.Enum:
-			return P.PatternEnumProperty.from(serialized);
-		case PatternPropertyType.Number:
-			return P.PatternNumberProperty.from(serialized);
-		case PatternPropertyType.NumberArray:
-			return P.PatternNumberArrayProperty.from(serialized);
-		case PatternPropertyType.Object:
-			return P.PatternObjectProperty.from(serialized);
-		case PatternPropertyType.String:
-			return P.PatternStringProperty.from(serialized);
-		case PatternPropertyType.StringArray:
-			return P.StringPatternArrayProperty.from(serialized);
-	}
-}; */

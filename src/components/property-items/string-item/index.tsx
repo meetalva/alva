dfaf09b5c70c2a8f2be26a32c174948de4b5652c
Mario Nebl <hello@mario-nebl.de>
2018-05-24 16:27:29 +0200

    fix: clean up borked rebase

--- src/components/property-items/string-item/index.tsx
+++ src/components/property-items/string-item/index.tsx
@@ -62,11 +62,7 @@ export const StringItem: React.StatelessComponent<StringItemProps> = props => {
 				onChange={onChange}
 				onBlur={onBlur}
 				type="text"
-<<<<<<< HEAD:src/components/property-items/string-item/index.tsx
 				value={value || ''}
-=======
-				value={value}
->>>>>>> b5c1963... feat: update styling of property pane (#436):src/lsg/patterns/property-items/string-item/index.tsx
 				placeholder="…"
 			/>
 		</StyledStringItem>

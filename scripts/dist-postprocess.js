0b3b5982a4cd325cb0dd397c2fd54f6e0c9c295f
Lasse Kuechler <lasse.kuechler@sinnerschrader.com>
2018-05-30 14:55:34 +0200

    build(distribution): remove copy command for none existing files

--- scripts/dist-postprocess.js
+++ scripts/dist-postprocess.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 execSync(`
 	mkdir upload-for-github-release &&
-	cp Alva-* alva_* latest* github/* upload-for-github-release/ &&
+	cp Alva-* latest* github/* upload-for-github-release/ &&
 	rm upload-for-github-release/Alva-*.blockmap
 `, {
 		cwd: 'dist'

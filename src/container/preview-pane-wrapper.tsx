3d7a4b4afbd53222a36b473c3d81bf69f04492ef
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-06-06 15:31:12 +0200

    feat(overlay): implementation of the overlay component

--- src/container/preview-pane-wrapper.tsx
+++ src/container/preview-pane-wrapper.tsx
@@ -3,7 +3,13 @@ import { PreviewFrame, PreviewPane } from '../components';
 import { PreviewDocumentMode } from '../preview';
 import * as React from 'react';
 
+import { Copy, CopySize } from '../components/copy';
+import { Icon, IconName, IconSize } from '../components/icons';
+import { Overlay } from '../components/overlay';
+import { Space, SpaceSize } from '../components/space';
+
 export interface PreviewPaneProps {
+	isDragging: boolean;
 	previewFrame: string;
 }
 
@@ -17,6 +23,12 @@ export class PreviewPaneWrapper extends React.Component<PreviewPaneProps> {
 					src={setSearch(props.previewFrame, { mode: PreviewDocumentMode.Live })}
 					offCanvas={false}
 				/>
+				<Overlay isVisisble={props.isDragging}>
+					<Space size={[0, 0, SpaceSize.L]}>
+						<Icon name={IconName.Robo} size={IconSize.S} />
+					</Space>
+					<Copy size={CopySize.M}>Drop the component on the left element list</Copy>
+				</Overlay>
 			</PreviewPane>
 		);
 	}

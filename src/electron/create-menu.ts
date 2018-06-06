3a165e96c0c7a8a7e41c286b2b04eaaad02b9c16
Lasse Kuechler <lasse.kuechler@sinnerschrader.com>
2018-06-06 14:36:23 +0200

    fixup! change alert to dialog

--- src/electron/create-menu.ts
+++ src/electron/create-menu.ts
@@ -114,9 +114,12 @@ export function createMenu(ctx: MenuContext): void {
 									return;
 								}
 
-								alert(
-									'Before you can open an exported Sketch-File:\n\n(1) Download & Install "Almost Sketch Plugin": https://github.com/brainly/html-sketchapp/releases/latest\n\n(2) Open Sketch, run "Plugins > From Almost Sketch to Sketch" and select exported file\n\nWe are currently working on a smoother experience.'
-								);
+								dialog.showMessageBox({
+									type: 'info',
+									message:
+										'Before you can open an exported Sketch-File:\n\n(1) Download & Install "Almost Sketch Plugin": https://github.com/brainly/html-sketchapp/releases/latest\n\n(2) Open Sketch, run "Plugins > From Almost Sketch to Sketch" and select exported file\n\nWe are currently working on a smoother experience.',
+									buttons: ['ok', 'get more info']
+								});
 
 								const path = await queryPath({
 									title: 'Export Sketch as',

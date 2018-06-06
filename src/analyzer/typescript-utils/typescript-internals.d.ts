1a54ef045cb3b61436a576c049ec61f756bb6d5e
Mario Nebl <hello@mario-nebl.de>
2018-06-06 15:33:50 +0200

    style: use TypeScript types explicitly

--- src/analyzer/typescript-utils/typescript-internals.d.ts
+++ src/analyzer/typescript-utils/typescript-internals.d.ts
@@ -1,4 +1,4 @@
-import * as ts from 'typescript';
+import * as TypeScript from 'typescript';
 
 declare module 'typescript' {
 	interface TypeChecker {
@@ -6,17 +6,17 @@ declare module 'typescript' {
 		 * DANGER: TypeScript API marked as internal
 		 * https://github.com/Microsoft/TypeScript/blob/d9b93903c035e48c8da1d731332787f83efc4619/src/compiler/types.ts#L3062
 		 * */
-		isArrayLikeType(type: Type): boolean;
+		isArrayLikeType(type: TypeScript.Type): boolean;
 
 		/**
 		 * DANGER: TypeScript API marked as internal
 		 * https://github.com/Microsoft/TypeScript/blob/d9b93903c035e48c8da1d731332787f83efc4619/src/compiler/types.ts#L3076
 		 * */
 		getAccessibleSymbolChain(
-			symbol: Symbol,
-			enclosingDeclaration: Node | undefined,
-			meaning: SymbolFlags,
+			symbol: TypeScript.Symbol,
+			enclosingDeclaration: TypeScript.Node | undefined,
+			meaning: TypeScript.SymbolFlags,
 			useOnlyExternalAliasing: boolean
-		): Symbol[] | undefined;
+		): TypeScript.Symbol[] | undefined;
 	}
 }

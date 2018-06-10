ee30868867855571a4ba31f50784ae749fb82aa1
Mario Nebl <hello@mario-nebl.de>
2018-06-10 08:18:41 +0200

    fix: unify drag image style and handling

--- src/container/pattern-list/pattern-list-container.tsx
+++ src/container/pattern-list/pattern-list-container.tsx
@@ -1,4 +1,5 @@
 import { Search, Space, SpaceSize } from '../../components';
+import { ElementDragImage } from '../element-drag-image';
 import * as MobxReact from 'mobx-react';
 import { PatternFolderContainer } from './pattern-folder-container';
 import { PatternItemContainer } from './pattern-item-container';
@@ -8,6 +9,15 @@ import { ViewStore } from '../../store';
 @MobxReact.inject('store')
 @MobxReact.observer
 export class PatternListContainer extends React.Component {
+	private dragImg: HTMLElement | null;
+
+	private handleDragStart(e: React.DragEvent<HTMLElement>): void {
+		if (this.dragImg) {
+			e.dataTransfer.effectAllowed = 'copy';
+			e.dataTransfer.setDragImage(this.dragImg, 75, 15);
+		}
+	}
+
 	public render(): JSX.Element | null {
 		const { store } = this.props as { store: ViewStore };
 		const patternLibrary = store.getPatternLibrary();
@@ -20,7 +30,7 @@ export class PatternListContainer extends React.Component {
 		const matches = patternLibrary.query(store.getPatternSearchTerm());
 
 		return (
-			<>
+			<div onDragStart={e => this.handleDragStart(e)}>
 				<Space sizeBottom={SpaceSize.XXS}>
 					<Search
 						placeholder="Search Library"
@@ -34,7 +44,11 @@ export class PatternListContainer extends React.Component {
 					matches={matches}
 					render={pattern => <PatternItemContainer key={pattern.getId()} pattern={pattern} />}
 				/>
-			</>
+				<ElementDragImage
+					element={store.getDraggedElement()}
+					innerRef={ref => (this.dragImg = ref)}
+				/>
+			</div>
 		);
 	}
 }

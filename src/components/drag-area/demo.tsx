5264bd0b9bd68f994f2776ab588e0a4aa5e92634
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-06-20 16:50:01 +0200

    feat(drag-area): inlude component methods

--- src/components/drag-area/demo.tsx
+++ src/components/drag-area/demo.tsx
@@ -5,7 +5,9 @@ import { DragArea } from './';
 
 export const DemoDragArea: React.SFC<{}> = (): JSX.Element => (
 	<DemoContainer>
-		<DragArea>Drag Area</DragArea>
+		<DragArea onDragStart={e => console.log(e)}>
+			<div draggable>Drag Area Element</div>
+		</DragArea>
 	</DemoContainer>
 );
 
--- src/components/drag-area/index.tsx
+++ src/components/drag-area/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+export interface DragAreaProps {
+	onDragStart: React.DragEventHandler<HTMLElement>;
+}
+
 const StyledDragArea = styled.div`
 	width: 100%;
 	height: 100%;
 `;
 
-export const DragArea: React.SFC<{}> = props => (
-	<StyledDragArea>
-		{props.children}
-		Hello World
-	</StyledDragArea>
+export const DragArea: React.SFC<DragAreaProps> = props => (
+	<StyledDragArea onDragStart={props.onDragStart}>{props.children}</StyledDragArea>
 );

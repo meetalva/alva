fc1c8fe2971fc909cdb65aa0e16157bdd6bee3ba
Mario Nebl <hello@mario-nebl.de>
2018-07-13 11:33:35 +0200

    feat: set search to sticky

--- src/container/pattern-list/pattern-list-container.tsx
+++ src/container/pattern-list/pattern-list-container.tsx
@@ -4,6 +4,7 @@ import * as MobxReact from 'mobx-react';
 import * as Model from '../../model';
 import { PatternItemContainer } from './pattern-item-container';
 import * as React from 'react';
+import styled from 'styled-components';
 import * as Types from '../../types';
 import { ViewStore } from '../../store';
 
@@ -31,27 +32,39 @@ export class PatternListContainer extends React.Component {
 		const searchResult = project.getPatternSearch().query(store.getPatternSearchTerm());
 
 		return (
-			<div onDragStart={e => this.handleDragStart(e)}>
-				<Components.Space sizeBottom={Components.SpaceSize.XXS}>
-					<Components.Search
-						placeholder="Search Library"
-						onChange={e => store.setPatternSearchTerm(e.target.value)}
-						value={store.getPatternSearchTerm()}
-					/>
-				</Components.Space>
-				{store
-					.getPatternLibraries()
-					.map(library => (
-						<PatternLibraryContainer
-							key={library.getId()}
-							library={library}
-							searchResult={searchResult}
+			<div>
+				<div
+					style={{
+						position: 'sticky',
+						top: 0,
+						backgroundColor: Components.Color.Grey97,
+						marginLeft: -1 * Components.getSpace(Components.SpaceSize.M),
+						marginRight: -1 * Components.getSpace(Components.SpaceSize.M)
+					}}
+				>
+					<Components.Space sizeBottom={Components.SpaceSize.XXS}>
+						<Components.Search
+							placeholder="Search Library"
+							onChange={e => store.setPatternSearchTerm(e.target.value)}
+							value={store.getPatternSearchTerm()}
 						/>
-					))}
-				<ElementDragImage
-					element={store.getDraggedElement()}
-					innerRef={ref => (this.dragImg = ref)}
-				/>
+					</Components.Space>
+				</div>
+				<div onDragStart={e => this.handleDragStart(e)} style={{}}>
+					{store
+						.getPatternLibraries()
+						.map(library => (
+							<PatternLibraryContainer
+								key={library.getId()}
+								library={library}
+								searchResult={searchResult}
+							/>
+						))}
+					<ElementDragImage
+						element={store.getDraggedElement()}
+						innerRef={ref => (this.dragImg = ref)}
+					/>
+				</div>
 			</div>
 		);
 	}

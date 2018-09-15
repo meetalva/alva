8c4d13ae898fbd502873bcaafae5de32a9bfffda
Tilman Frick <git@tilman.io>
2018-09-15 21:38:53 +0200

    fix: remove unneeded element list scroll area

--- src/components/drag-area/drag-area.tsx
+++ src/components/drag-area/drag-area.tsx
@@ -30,6 +30,9 @@ export interface DragAreaProps {
 const StyledDragArea = styled.div`
 	width: 100%;
 	height: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
 `;
 
 export const DragArea: React.SFC<DragAreaProps> = props => (

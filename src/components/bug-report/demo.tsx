79591b3f08c0864d76bdacc8b53a5c96dcc7235c
Tilman Frick <git@tilman.io>
2018-09-15 19:38:34 +0200

    feat: update styling of bug report button

--- src/components/bug-report/demo.tsx
+++ src/components/bug-report/demo.tsx
@@ -1,11 +1,8 @@
-import DemoContainer from '../demo-container';
 import { BugReport } from './index';
 import * as React from 'react';
 
 const BugReportDemo: React.StatelessComponent<void> = (): JSX.Element => (
-	<DemoContainer title="BugReport">
-		<BugReport title="Found a bug?" />
-	</DemoContainer>
+	<BugReport title="Found a bug?" />
 );
 
 export default BugReportDemo;
--- src/components/bug-report/index.tsx
+++ src/components/bug-report/index.tsx
@@ -1,6 +1,7 @@
-import { Button, ButtonOrder, ButtonSize } from '../button';
 import * as React from 'react';
 import styled from 'styled-components';
+import { Color } from '../colors';
+import { getSpace, SpaceSize } from '../space';
 
 export interface BugReportProps {
 	onClick?: React.MouseEventHandler<HTMLElement>;
@@ -9,19 +10,25 @@ export interface BugReportProps {
 }
 
 const StyledBugReport = styled.div`
+	display: flex;
+	padding: 0 ${getSpace(SpaceSize.S)}px;
+	align-items: center;
 	justify-self: right;
 	-webkit-app-region: no-drag;
+	box-sizing: border-box;
+	border-radius: 3px;
+	background: linear-gradient(to bottom, ${Color.White} 0%, ${Color.Grey97});
+	height: 21px;
+	box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.1), 0 0.5px 2px 0 rgba(0, 0, 0, 0.3);
+	color: ${Color.Grey50};
+
+	&:active {
+		background: ${Color.Grey90};
+	}
 `;
 
 export const BugReport: React.StatelessComponent<BugReportProps> = props => (
-	<StyledBugReport>
-		<Button
-			order={ButtonOrder.Secondary}
-			size={ButtonSize.Small}
-			onClick={props.onClick}
-			onDoubleClick={props.onDoubleClick}
-		>
-			{props.title}
-		</Button>
+	<StyledBugReport onClick={props.onClick} onDoubleClick={props.onDoubleClick} title={props.title}>
+		{props.title}
 	</StyledBugReport>
 );

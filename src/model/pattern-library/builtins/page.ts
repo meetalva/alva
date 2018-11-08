8f9983ebd7ef36a4fce153f97666e2d619afc14f
Tilman Frick <git@tilman.io>
2018-11-08 22:06:51 +0100

    fix: remove page properties for clarity

--- src/model/pattern-library/builtins/page.ts
+++ src/model/pattern-library/builtins/page.ts
@@ -1,61 +1,13 @@
-import { languages } from './languages';
 import { Pattern, PatternSlot } from '../../pattern';
 import { BuiltInContext, BuiltInResult } from '../pattern-library';
-import {
-	PatternBooleanProperty,
-	PatternEnumProperty,
-	PatternEnumPropertyOption
-} from '../../pattern-property';
 import * as Types from '../../../types';
 
 const PATTERN_CONTEXT_ID = 'synthetic:page';
 const SLOT_CONTEXT_ID = 'children';
-const VIEWPORT_CONTEXT_ID = 'viewport';
-const LANG_CONTEXT_ID = 'lang';
 
 export const Page = (context: BuiltInContext): BuiltInResult => {
 	const patternId = context.options.getGlobalPatternId(PATTERN_CONTEXT_ID);
-	const langEnumId = context.options.getGlobalPropertyId(patternId, LANG_CONTEXT_ID);
-
-	const options = languages.map(
-		(language, index) =>
-			new PatternEnumPropertyOption({
-				contextId: language.value,
-				icon: undefined,
-				id: context.options.getGlobalEnumOptionId(langEnumId, language.value),
-				name: language.name,
-				ordinal: '1',
-				value: language.value
-			})
-	);
-
-	const defaultLanguage = options.find(option => option.getValue() === 'en');
-
-	const properties = [
-		new PatternBooleanProperty({
-			contextId: VIEWPORT_CONTEXT_ID,
-			description: 'Adapt viewport to device',
-			id: context.options.getGlobalPropertyId(patternId, VIEWPORT_CONTEXT_ID),
-			inputType: Types.PatternPropertyInputType.Default,
-			label: 'Mobile Viewport',
-			origin: Types.PatternPropertyOrigin.BuiltIn,
-			propertyName: 'viewport',
-			defaultValue: true
-		}),
-		new PatternEnumProperty({
-			contextId: LANG_CONTEXT_ID,
-			defaultOptionId: defaultLanguage ? defaultLanguage.getId() : undefined,
-			hidden: false,
-			id: langEnumId,
-			inputType: Types.PatternPropertyInputType.Select,
-			label: 'Language',
-			description: 'Define in which language your website is.',
-			origin: Types.PatternPropertyOrigin.BuiltIn,
-			options,
-			propertyName: 'lang',
-			required: false
-		})
-	];
+	const properties = [];
 
 	const pattern = new Pattern(
 		{
@@ -65,7 +17,7 @@ export const Page = (context: BuiltInContext): BuiltInResult => {
 			id: patternId,
 			name: 'Page',
 			origin: Types.PatternOrigin.BuiltIn,
-			propertyIds: properties.map(p => p.getId()),
+			propertyIds: properties,
 			slots: [
 				new PatternSlot({
 					contextId: 'children',

30f979faae018eb2f036bf4523f70bedc86e6cfd
Mario Nebl <hello@mario-nebl.de>
2018-11-10 11:08:20 +0100

    fix: appease tslint

--- src/renderer/fallback-host.ts
+++
@@ -1,7 +0,0 @@
-import * as Types from '../types';
-
-/**
- * Fallback host implementing functionalites that
- * may not be available without Electron by using browser features directly
- */
-export class FallbackHost implements Partial<Types.Host> {}

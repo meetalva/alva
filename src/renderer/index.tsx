e71853faf7e0f932650fb74eda519c994209744f
Mario Nebl <hello@mario-nebl.de>
2018-11-18 16:33:09 +0100

    feat: take a stab at ios support

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,3 +1,9 @@
 import { startRenderer } from './renderer';
+import * as MobileDnD from 'mobile-drag-drop';
 
+// Convince iOS to collaborate with mobile-dnd
+// tslint:disable-next-line:no-empty
+window.addEventListener('touchmove', () => {});
+
+MobileDnD.polyfill();
 startRenderer();

a38c8fe1a69a28578513f32a67f35234347eca4c
Mario Nebl <marionebl@users.noreply.github.com>
2018-12-07 01:20:52 +0100

    fix: prevent clicking between text lines #651 (#671)

--- src/preview-renderer/synthetic-text.tsx
+++ src/preview-renderer/synthetic-text.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
 
 // tslint:disable-next-line:no-any
-export const SyntheticText: React.SFC<any> = props => <span>{props.text}</span>;
+export const SyntheticText: React.SFC<any> = props => (
+	<span style={{ display: 'inline-block' }}>{props.text}</span>
+);

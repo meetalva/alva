19bb5585d8c7a32ee291feee021b81d4cdcceac5
Mario Nebl <hello@mario-nebl.de>
2019-01-15 20:06:14 +0100

    chore: move test tooling to root

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+module.exports = {
+	preset: "ts-jest",
+	testMatch: ["**/*.test.ts", "**/*.test.tsx"],
+	roots: [
+		"packages/core/",
+		"packages/essentials",
+		"packages/loader",
+		"packages/site",
+		"packages/tools"
+	],
+	testPathIgnorePatterns: [
+		"node_modules",
+		"lib"
+	],
+	globals: {
+		"ts-jest": {
+			diagnostics: false
+		}
+	}
+};

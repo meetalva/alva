ecbc4b36159c2f95ca995255a65e12378a0af201
Mario Nebl <hello@mario-nebl.de>
2019-01-29 23:32:15 +0100

    chore: set artifact name based on channel

--- packages/tools/alva-trigger.js
+++ packages/tools/alva-trigger.js
@@ -177,10 +177,18 @@ async function main(cli) {
 
 		if (channel === 'pr') {
 			manifest.build.productName = `Alva PR ${prNumber}`;
+			manifest.build.mac.artifactName = `alva-\${version}-pr-${prNumber}.${ext}`;
+			manifest.build.nsis.artifactName = `alva-\${version}-pr-${prNumber}.${ext}`;
+			manifest.build.portable.artifactName = `alva-portable-\${version}-pr-${prNumber}.${ext}`;
+			manifest.build.linux.artifactName = `alva-\${version}-pr-${prNumber}.${ext}`;
 		}
 
 		if (channel === 'alpha') {
 			manifest.build.productName = `Alva Canary`;
+			manifest.build.mac.artifactName = 'alva-${version}-canary.${ext}';
+			manifest.build.nsis.artifactName = 'alva-${version}-canary.${ext}';
+			manifest.build.portable.artifactName = 'alva-portable-${version}-canary.${ext}';
+			manifest.build.linux.artifactName = 'alva-${version}-canary.${ext}';
 		}
 
 		await writeFile(

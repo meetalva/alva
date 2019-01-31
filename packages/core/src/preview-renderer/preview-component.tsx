a8223b71528fa9d7ea4eee84a8b85de564a7e1b9
Tilman Frick <git@tilman.io>
2019-01-31 15:20:08 +0100

    fix: allow components to pass properties (#742)

--- packages/core/src/preview-renderer/preview-component.tsx
+++ packages/core/src/preview-renderer/preview-component.tsx
@@ -104,6 +104,7 @@ export class PreviewComponent extends React.Component<PreviewComponentProps> {
 
 	public render(): JSX.Element | null {
 		const props = this.props as Injected;
+		const { store, element, ...other } = this.props as Injected;
 
 		const children = props.store.getChildren(props.element, child => (
 			<PreviewComponent key={child.getId()} element={child} />
@@ -121,8 +122,8 @@ export class PreviewComponent extends React.Component<PreviewComponentProps> {
 		}
 
 		return (
-			<PreviewComponentError name={props.element.getName()}>
-				<Component {...properties} {...slots}>
+			<PreviewComponentError name={props.element.getName()} {...other}>
+				<Component {...properties} {...other} {...slots}>
 					{children}
 				</Component>
 			</PreviewComponentError>

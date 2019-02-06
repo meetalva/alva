cd704e19c5ffa1167305843b683f324b1565b300
Tilman Frick <git@tilman.io>
2019-02-06 16:02:01 +0100

    feat: update recommended libraries

--- packages/site/src/site.tsx
+++ packages/site/src/site.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import * as D from '@meetalva/designkit';
+import * as D from '@meetalva/alva-design';
 import { Releases } from './releases';
 
 export * from './render';
